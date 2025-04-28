17968f2ec3d53781b5c5b625d6936b5aa5ce8875
Euromir <viniciusrubim2015@gmail.com>
2025-04-28 09:27:28 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController2 : MonoBehaviour
+{
+    Camera cam;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
